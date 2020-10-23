f5243243f622322a188f6a12918e5c7ea08a4f87
GROUSSAUD Axel <groussaudaxel@gmail.com>
2020-10-23 11:27:52 +0200

    classe reservation finis et testé

--- ReservTechLSRGames/Reservation.cs
+++ ReservTechLSRGames/Reservation.cs
@@ -8,5 +8,59 @@ namespace ReservTechLSRGames
 {
     class Reservation
     {
+        private int _idReservation;
+        private int _idClient;
+        private int _idSalle;
+        private int _idTransaction;
+        private DateTime _jour;
+        private DateTime _heure;
+        private int _nbJoueurs;
+        private int _nbObstacle;
+
+        public Reservation()
+        {
+            _idReservation = 0;
+            _idClient = 0;
+            _idSalle = 0;
+            _idTransaction = 0;
+            _jour = new DateTime();
+            _heure = new DateTime();
+            _nbJoueurs = 0;
+            _nbObstacle = 0;
+        }
+
+        public Reservation (int unIdReservation, int unIdClient, int unIdSalle, int unIdTransaction, DateTime unJour, DateTime uneHeure,int unNbJoueurs, int unNbObstacle)
+        {
+            _idReservation = unIdReservation;
+            _idClient = unIdClient;
+            _idSalle = unIdSalle;
+            _idTransaction = unIdTransaction;
+            _jour = unJour;
+            _heure = uneHeure;
+            _nbJoueurs = unNbJoueurs;
+            _nbObstacle = unNbObstacle;
+        }
+
+        public int IdReservation { get => _idReservation; set => _idReservation = value; }
+
+        public int IdClient { get => _idClient; set => _idClient = value; }
+
+        public int IdSalle { get => _idSalle; set => _idSalle = value; }
+
+        public int IdTransaction { get => _idTransaction; set => _idTransaction = value; }
+
+        public DateTime getJour()
+        {
+            return _jour;
+        }
+
+        public void setJour(DateTime unJour)
+        {
+            _jour = unJour;
+        }
+
+        public int NbJoueurs { get => _nbJoueurs; set => _nbJoueurs = value; }
+
+        public int NbObstacle { get => _nbObstacle; set => _nbObstacle = value; }
     }
 }
